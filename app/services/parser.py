b08130ddfef9693905b12a7e622f17b07bb93b56
Dmitriy-Bitsenko <bitsenko@inbox.ru>
2026-05-31 21:05:19 +0300

    feat(parser): Implement initial structure for HTML parser

---
+++ app/services/parser.py
@@ -0,0 +1,46 @@
+
+import aiohttp
+import asyncio
+from bs4 import BeautifulSoup
+
+
+
+URL = "https://www.ufanet.ru/#"
+
+async def fetch_url(url: str) -> str:
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            response.raise_for_status()
+            html_text = await response.text()
+            return html_text
+
+
+def parse_tariffs_from_html(html: str):
+    soup = BeautifulSoup(html, 'html.parser')
+    all_target_block_links = soup.find_all("ul", class_="list")
+    target_ul_block = all_target_block_links[0]
+    list_links = []
+    list_items_in_block = target_ul_block.find_all('li')
+
+    for item in list_items_in_block:
+        anchor_tag = item.find('a')
+        if anchor_tag:
+            href_value = anchor_tag['href']
+            list_links.append(href_value)
+    return list_links
+
+
+
+
+async def main():
+    result_fetch_url = await fetch_url(URL)
+    result_parse_tariffs_from_html = parse_tariffs_from_html(result_fetch_url)
+    print(result_parse_tariffs_from_html)
+
+
+
+
+if __name__ == "__main__":
+
+    asyncio.run(main())
+
