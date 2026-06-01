59788ff8aef81565c9f367c0c7fb7d4e31f5d7d4
Dmitriy-Bitsenko <bitsenko@inbox.ru>
2026-06-01 22:48:00 +0300

    feat: Update tariff parser and project plan

--- app/services/parser.py
+++ app/services/parser.py
@@ -26,16 +26,25 @@ def parse_tariffs_from_html(html: str):
         anchor_tag = item.find('a')
         if anchor_tag:
             href_value = anchor_tag['href']
-            list_links.append(href_value)
+            list_links.append(f'https://www.ufanet.ru{href_value}')
     return list_links
 
 
+async def parse_detailed_tariff_page(tarif_url):
+    # for link in links:
+    #     name = link.find("div", class_='text')
+    #     return name
+        pass 
 
 
 async def main():
     result_fetch_url = await fetch_url(URL)
     result_parse_tariffs_from_html = parse_tariffs_from_html(result_fetch_url)
-    print(result_parse_tariffs_from_html)
+    # print(result_parse_tariffs_from_html)
+    list_of_links = parse_tariffs_from_html(result_fetch_url)
+    tarif_url = pass
+    parse_detailed_tariff_page(list_of_links)
+
 
 
 
